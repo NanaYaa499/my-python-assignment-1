378f647ac7b19069d95a73088a4eb30204facf44
NanaYaa499 <dorisnyame06@gmail.com>
2025-08-04 13:36:51 +0000

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+print(f"{num1} + {num2} = ", num1 + num2)
+print(f"{num1} - {num2} = ", num1 - num2)
+print(f"{num1} * {num2} = ", num1 * num2)
+print(f"{num1} / {num2} = ", num1 / num2)
